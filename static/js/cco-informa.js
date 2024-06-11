42c6e74edf5c8ac06a98f6533ff7acaafc7f04a7
AdryanMaikel <adryanmaikel@gmail.com>
2024-06-11 00:05:09 -0300

    criando efeito de edição

--- static/js/cco-informa.js
+++ static/js/cco-informa.js
@@ -8,3 +8,32 @@ const table = document.querySelector("table")
 function adjust_width_table(width){table.style.width = width <= width_columns.sum?`100%`:`${width_columns.sum + 10}px`}
 adjust_width_table(window.innerWidth);
 window.addEventListener("resize", event=>adjust_width_table(event.target.innerWidth));
+
+// const open_config_sheet = document.querySelector("#div-config-table")
+// open_config_sheet.onclick = (event)=>{const element = event.target;element.classList.toggle("true");element.parentElement.classList.toggle("open")}
+
+var editing = {this: false, element: HTMLElement}
+var focus = false
+
+const trs = document.querySelectorAll("tbody > tr")
+
+function check_editing(target) {
+    if(editing.this && editing.element.id == target.id){
+        return;
+    }
+    if(!focus){target.classList.add("focus");focus = true;return;}
+    active_editing(target);
+}
+
+function active_editing(target) {
+    if(editing.this)return;
+    editing.this=true;
+    console.log("ativando linha "+target.id)
+    target.classList.add("active");
+    target.querySelectorAll("td textarea").forEach(textarea=>textarea.disabled = false)
+}
+
+trs.forEach(tr=>{
+    tr.addEventListener("click", ({target})=>check_editing(target))
+    tr.addEventListener("dblclick", ({target})=>active_editing(target))
+})
