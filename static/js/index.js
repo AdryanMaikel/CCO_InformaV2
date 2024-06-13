74bdbdeadec8acab49edb5cec5fe2fdc7198f0dd
NGS-Adryan <ngs.adryan@gmail.com>
2024-06-13 17:32:57 -0300

    adicionando login
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,21 @@
-document.getElementById("config").onclick = _=>document.getElementById("menu-config").classList.add("open")
-document.getElementById("config-close").onclick = _=>document.getElementById("menu-config").classList.remove("open")
+const menu_config = document.getElementById("menu-config");
+document.getElementById("config-open").onclick = () => menu_config.classList.add("open");
+document.getElementById("config-close").onclick = () => menu_config.classList.remove("open");
 
 const overlay = document.getElementById("overlay");
 const iframe = document.querySelector("iframe");
-iframe.addEventListener("load", _=>overlay.classList.add("hidden"));
-window.addEventListener("message", _=>overlay.classList.remove("hidden"))
+// iframe.addEventListener("load", _=>overlay.classList.add("hidden"));
+// window.addEventListener("message", _=>overlay.classList.remove("hidden"))
+iframe.onload = () => overlay.classList.add("hidden");
+window.onmessage = () => overlay.classList.remove("hidden");
+
+const login = document.getElementById("login");
+const cancel_login = document.getElementById("login-cancel");
+
+
+document.getElementById("user").onclick = () => {
+    login.classList.add("open");
+
+
+    cancel_login.onclick = () => login.classList.remove("open");
+}
