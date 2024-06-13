f46fffdb0a6868f40bff3dd615a8704fbed87d11
NGS-Adryan <ngs.adryan@gmail.com>
2024-06-13 17:32:48 -0300

    refatorando cco-informa
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- static/js/cco-informa.js
+++ static/js/cco-informa.js
@@ -1,17 +1,19 @@
 import { width_columns, update_cookie_width_columns } from "./cco-informa-cookies.js";
 
-function set_value_in_property(property, value){document.documentElement.style.setProperty(property, `${value}`)}
-function sum_columns(){Object.entries(width_columns).forEach(([column, width])=>{set_value_in_property(`--${column}`, `${width}px`);width_columns.sum += width;})}
+function sum_columns() {
+    Object.entries(width_columns).forEach(([column, width]) => {
+        document.documentElement.style.setProperty(`--${column}`, `${width}px`);
+        width_columns.sum += width;
+    })
+}
 sum_columns()
 
 const table = document.querySelector("table")
-function adjust_width_table(width){table.style.width = width <= width_columns.sum?`100%`:`${width_columns.sum + 10}px`}
+function adjust_width_table(width) {
+    table.style.width = width <= width_columns.sum ? `100%` : `${width_columns.sum + 10}px`;
+}
 adjust_width_table(window.innerWidth);
-window.addEventListener("resize", event=>adjust_width_table(event.target.innerWidth));
-
-// const open_config_sheet = document.querySelector("#div-config-table")
-// open_config_sheet.onclick = (event)=>{const element = event.target;element.classList.toggle("true");element.parentElement.classList.toggle("open")}
-
+window.onresize = ({target}) => adjust_width_table(target.innerWidth);
 
 const url = window.location.href;
 
@@ -21,61 +23,62 @@ var focus = {this: false, element: HTMLElement}
 
 const buttons_actions = document.getElementById("actions-row")
 
-function check_editing(target) {
-    if(editing.this && editing.element.id != target.id)return;
-    if(focus.this){
-        if(focus.element == target){
-            target.classList.add("active");
-            active_editing(target);
-            return
-        }else{
-            focus.element.classList.remove("focus")
-        }
-    }
-    focus.element = target;target.classList.add("focus");focus.this = true;buttons_actions.classList.add("focus");return;
-}
-
-function active_editing(target) {
-    if(editing.this)return;
-    buttons_actions.classList.remove("focus");
-    buttons_actions.classList.add("confirm");
-    editing.this = true;editing.element = target;
+function active_editing(target, action) {
+    if(editing.this || !focus.this) return;
+    buttons_actions.classList.replace("focus", "confirm");
+    target.classList.replace("focus", action == "del" ? "delete" : "active");
+    editing.this = true;
+    editing.element = target;
+    editing.action = action;
     target.querySelectorAll("td").forEach(td=>{
         const textarea = td.querySelector("textarea");
-        if(!textarea){editing.values = {row: parseInt(td.textContent.trim())};return}
-        editing.values[td.className] = textarea.value;textarea.disabled = false;
+        if(!textarea) {
+            editing.values = { row: parseInt(td.textContent.trim()) };
+            return;
+        }
+        if(action == "del") return;
+        editing.values[td.className] = textarea.value;
+        textarea.disabled = false;
+        if(action == "add") textarea.value = "";
     })
+    // console.log(editing)
+}
+
+function check_editing(target) {
+    if(editing.this && editing.element.id != target.id) return;
+    if(focus.this) {
+        if(focus.element == target) {
+            active_editing(target, "edit");
+            return;
+        }
+        focus.element.classList.remove("focus");
+    }
+    focus.element = target;
+    target.classList.add("focus");
+    focus.this = true;
+    buttons_actions.classList.add("focus");
 }
 
 document.querySelectorAll("tbody > tr").forEach(tr=>{
-    tr.addEventListener("click", ({target})=>check_editing(target))
+    tr.onclick = ({target}) => check_editing(target);
 })
 
-document.getElementById("del-row").addEventListener("click", _=>{
-    if(!focus.this)return;
-    active_editing(focus.element);
-    editing.action = "del";
-    focus.element.classList.remove("active");
-    focus.element.classList.add("delete");
-})
+document.getElementById("del-row").onclick = () => active_editing(focus.element, "del")
+document.getElementById("edt-row").onclick = () => active_editing(focus.element, "edit")
+document.getElementById("add-row").onclick = () => active_editing(focus.element, "add")
 
-document.getElementById("cancel-editing").addEventListener("click", _=>{
-    if(!editing.this)return;
-    editing.element.querySelectorAll("td").forEach(td=>{
-        const textarea = td.querySelector(`textarea`);
-        if(!textarea){editing.values = {row: parseInt(td.textContent.trim())};return}
-        textarea.value = editing.values[td.className]
-    })
+function reset_layout() {
+    if(!editing.this) return;
+    if(editing.action == "edit" || editing.action == "add") {
+        editing.element.querySelectorAll("td textarea").forEach(textarea=>{
+            textarea.value = editing.values[textarea.parentElement.className]
+            textarea.disabled = true;
+        })
+    }
     editing.element.classList.remove("delete", "active", "focus");
-    buttons_actions.classList.remove("confirm");
+    buttons_actions.classList.remove("focus", "confirm");
     editing = {this: false, element: HTMLElement, values: {}, action: ""};
     focus = {this: false, element: HTMLElement};
-})
+}
 
-document.getElementById("edit-row").addEventListener("click", _=>{
-    if(!focus.this)return;
-    focus.element.classList.add("active");
-    active_editing(focus.element);
-    editing.action = "edit";
-    window.location.href = `${url}#${focus.element.id}`;
-})
+document.getElementById("cancel-editing").onclick = reset_layout;
