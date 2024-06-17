2b0a2d1ef8c3a3672af516ea71f3d63a3e0c40ba
NGS-Adryan <ngs.adryan@gmail.com>
2024-06-17 17:18:28 -0300

    criando sistema de login
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 """
 
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from gsheets import INFORMATIONS as SHEETS
+from db import contains_operator
 
 LETTERS = SHEETS["cco-informa"]["letters"]()
 COLUMNS = SHEETS["cco-informa"]["cols"]()
@@ -16,15 +17,19 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["POST"])
 def login():
-    return render_template("")
+    operator, password = request.form["operator"], request.form["password"]
+    if not contains_operator(operator, password):
+        return "not-found"
+    return f"cco-informa?operator={operator}"
 
 
 @app.route("/cco-informa")
 def cco_informa():
+    operator = request.args.get("operator")
     return render_template("cco-informa.html", letters=LETTERS, cols=COLUMNS,
-                           rows=SHEETS["cco-informa"]["get"]())
+                           rows=SHEETS["cco-informa"]["get"](), user=operator)
 
 
 @app.errorhandler(404)
