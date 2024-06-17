138cede8eefeac2d574641e1e1e6fff96e75d955
NGS-Adryan <ngs.adryan@gmail.com>
2024-06-17 17:19:27 -0300

    criando sistema de login
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- static/js/index.js
+++ static/js/index.js
@@ -4,18 +4,89 @@ document.getElementById("config-close").onclick = () => menu_config.classList.re
 
 const overlay = document.getElementById("overlay");
 const iframe = document.querySelector("iframe");
-// iframe.addEventListener("load", _=>overlay.classList.add("hidden"));
-// window.addEventListener("message", _=>overlay.classList.remove("hidden"))
+
 iframe.onload = () => overlay.classList.add("hidden");
-window.onmessage = () => overlay.classList.remove("hidden");
+
+window.onmessage = ({data}) => {
+    if(data == "reload"){
+        overlay.classList.remove("hidden");
+        setTimeout(()=>iframe.contentWindow.location.reload(), 500);
+    }
+};
 
 const login = document.getElementById("login");
-const cancel_login = document.getElementById("login-cancel");
 
+const operator = login.querySelector("#operator");
+const password = login.querySelector("#password");
 
-document.getElementById("user").onclick = () => {
+function reset_login(event = {}, operator_value = "", password_value = "") {
+    operator.disabled = true;
+    password.disabled = true;
+    bnt_submit_login.disabled = true;
+    btn_cancel_login.disabled = true;
+    operator.value = operator_value;
+    password.value = password_value;
+    login.classList.remove("open");
+}
+
+function create_form(action = "error", method = "POST", inputs = []) {
+    const form = document.createElement("form");
+    form.setAttribute("action", action);
+    form.setAttribute("method", method);
+    inputs.forEach(input=>form.appendChild(input.cloneNode(false)));
+    return form;
+}
+const btn_user = document.getElementById("user");
+
+async function check_login() {
+    overlay.classList.remove("hidden");
+    const form = create_form("/login", "POST", [operator, password]);
+    const form_data = new FormData(form);
+    try {
+        const response = await fetch("/login", {method: "POST", body: form_data});
+        const text = await response.text();
+        if(text == "not-found"){
+            login.classList.add("error");
+            return;
+        }
+        iframe.setAttribute("src", text);
+        login.classList.remove("open");
+        btn_user.classList.add("true");
+        const json_login = JSON.stringify({user: operator.value, password: password.value})
+        document.cookie=`login=${json_login}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const bnt_submit_login = login.querySelector("#submit-login");
+const btn_cancel_login = login.querySelector("#cancel-login");
+
+btn_user.onclick = () => {
+    if(btn_user.classList.contains("true")){
+        document.cookie.split(";").find(cookie=>{
+            if(cookie.trim().startsWith("login=")){
+                document.cookie=`login={}; expires=Fri, 31 Dec 1900 23:59:59 GMT; path=/`;
+            }
+        });
+        window.location.reload();
+        return;
+    }
+    operator.disabled = false;
+    password.disabled = false;
+    bnt_submit_login.disabled = false;
+    btn_cancel_login.disabled = false;
     login.classList.add("open");
+    bnt_submit_login.onclick = check_login;
+    btn_cancel_login.onclick = reset_login;
+}
 
 
-    cancel_login.onclick = () => login.classList.remove("open");
+if(document.cookie.split(";").find(cookie=>cookie.trim().startsWith("login="))){
+    const config = JSON.parse(decodeURIComponent(document.cookie.split(";").find(cookie=>cookie.trim().startsWith("login=")).split("=")[1]))
+    console.log("teste2")
+    operator.value = config.user;
+    password.value = config.password;
+    if(config.password && config.user)
+        check_login()
 }
